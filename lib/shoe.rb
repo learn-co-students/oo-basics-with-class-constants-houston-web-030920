508c4079f31184b725a277b36aa875a2c9b55869
melkilic <melike180400@gmail.com>
2020-03-15 10:28:24 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,19 @@
+require 'pry'
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-
+  attr_accessor :color, :size, :material, :condition , :brand
+  #you removed attr_reader for :brand and added it to attr_accessor since we want to be able to change the :brand. attr_reader wouldn't let us do that unless we had an attr_writer with it.
+   BRANDS = []
   def initialize(brand)
     @brand = brand
+    #you want to make sure that every brand is unique (dunno why couldn't use .uniq for this tho)=>
+   BRANDS << brand unless BRANDS.any?{|br| br == brand}
+    #you are keeping this part (BRANDS << brand) in this method unlike the book example because we want to access all the brands instead of a specific one. 
   end
+  
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+   
 end
